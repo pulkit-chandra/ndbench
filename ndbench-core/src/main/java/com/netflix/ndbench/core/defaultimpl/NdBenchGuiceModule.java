3dfbd456ef211e86e569acad59c7cb446c717a2a
Pulkit Chandra <pchandra@pivotal.io>
2017-06-22 14:55:58 -0400

    Add support for Geode and Cloud Foundry
     Updated
     1. Provided new discovery implementation for Cloud Foundry. Now you can
     push NdBench on Cloud Foundry and run.
     2. Added a new plugin for Geode in `local` and `cloud` mode.
     3. Updated read me on how to run NdBench on Cloud Foundry.

--- ndbench-core/src/main/java/com/netflix/ndbench/core/defaultimpl/NdBenchGuiceModule.java
+++ ndbench-core/src/main/java/com/netflix/ndbench/core/defaultimpl/NdBenchGuiceModule.java
@@ -32,6 +32,8 @@ import com.netflix.ndbench.core.monitoring.NdBenchDefaultMonitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//import com.netflix.ndbench.core.config.NdBenchConfiguration;
+
 /**
  * This module defines the default bindings, this can be customized internally if one has specific services for Service discovery, metrics etc.,
  * @author vchella
