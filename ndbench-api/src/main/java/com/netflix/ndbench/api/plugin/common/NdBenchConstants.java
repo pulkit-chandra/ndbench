060daa0179f1877037b641c66736210ad3fd02e0
cbedford <cbedford@netflix.com>
2017-07-24 19:35:36 -0700

    1) address comments on PR. 2) add unit test of RPSCount     -- added code from supporting library for verifying log4j activity -- by just copying it in (since the library is not yet available in maven binary repositories).

--- ndbench-api/src/main/java/com/netflix/ndbench/api/plugin/common/NdBenchConstants.java
+++ ndbench-api/src/main/java/com/netflix/ndbench/api/plugin/common/NdBenchConstants.java
@@ -45,7 +45,7 @@ public final class NdBenchConstants {
     public static final String WRITE_RATE_LIMIT="writeRateLimit";
 
     // Use constant so as to avoid hard coded string references in calling code
-    public static final String WRITE_RATE_LIMIT_FULL_NAME = PROP_NAMESPACE + WRITE_RATE_LIMIT;
+    public static final String WRITE_RATE_LIMIT_FULL_NAME = PROP_PREFIX + WRITE_RATE_LIMIT;
 
     public static final String CONFIG_CLUSTER_DISCOVERY_NAME="clusters.json";
 
