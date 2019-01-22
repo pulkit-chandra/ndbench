38a185f60b27d0f23cf5382a6601f817bbdb1362
Pulkit Chandra <pchandra@pivotal.io>
2019-01-22 13:34:32 -0500

    Rebased with master and also fixed log4j issues
    Rebased with master so we building on 0.5.0 release.
    Also, fixed the issue with log4j issues with war file created. Currently
    all the plugins are briging in their own log4j implementation when they
    are imported to web project. I have excluded the providers from all of
    them. This ensures that web is replying the plugins to provide its own
    log4j implementation and not expect the web module to carry any.

--- ndbench-core/src/test/java/org/libex/logging/log4j/InMemoryAppender.java
+++ ndbench-core/src/test/java/org/libex/logging/log4j/InMemoryAppender.java
@@ -1,15 +1,13 @@
 package org.libex.logging.log4j;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import javax.annotation.concurrent.ThreadSafe;
-
+import com.google.common.collect.ImmutableList;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
 
-import com.google.common.collect.ImmutableList;
+import javax.annotation.ParametersAreNonnullByDefault;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Appender that maintains the logging events in memory.
@@ -20,54 +18,54 @@ import com.google.common.collect.ImmutableList;
 @ParametersAreNonnullByDefault
 public class InMemoryAppender extends AppenderSkeleton {
 
-	private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
-	private final Lock readLock = rwLock.readLock();
-	private final Lock writeLock = rwLock.writeLock();
+    private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final Lock readLock = rwLock.readLock();
+    private final Lock writeLock = rwLock.writeLock();
 
-	private volatile ImmutableList.Builder<LoggingEvent> eventListBuilder = ImmutableList
-			.builder();
+    private volatile ImmutableList.Builder<LoggingEvent> eventListBuilder = ImmutableList
+            .builder();
 
-	@Override
-	public void close() {
-		// NO OP
-	}
+    @Override
+    public void close() {
+        // NO OP
+    }
 
-	@Override
-	public boolean requiresLayout() {
-		return false;
-	}
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
 
-	@Override
-	protected void append(LoggingEvent event) {
-		writeLock.lock();
-		try {
-			eventListBuilder.add(event);
-		} finally {
-			writeLock.unlock();
-		}
-	}
+    @Override
+    protected void append(LoggingEvent event) {
+        writeLock.lock();
+        try {
+            eventListBuilder.add(event);
+        } finally {
+            writeLock.unlock();
+        }
+    }
 
-	/**
-	 * @return the observed logging events
-	 */
-	public ImmutableList<LoggingEvent> getLoggingEvents() {
-		readLock.lock();
-		try {
-			return eventListBuilder.build();
-		} finally {
-			readLock.unlock();
-		}
-	}
+    /**
+     * @return the observed logging events
+     */
+    public ImmutableList<LoggingEvent> getLoggingEvents() {
+        readLock.lock();
+        try {
+            return eventListBuilder.build();
+        } finally {
+            readLock.unlock();
+        }
+    }
 
-	/**
-	 * Clears the list of observed logging events
-	 */
-	public void clear() {
-		writeLock.lock();
-		try {
-			eventListBuilder = ImmutableList.builder();
-		} finally {
-			writeLock.unlock();
-		}
-	}
+    /**
+     * Clears the list of observed logging events
+     */
+    public void clear() {
+        writeLock.lock();
+        try {
+            eventListBuilder = ImmutableList.builder();
+        } finally {
+            writeLock.unlock();
+        }
+    }
 }
